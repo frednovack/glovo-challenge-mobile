e183e505cfdc42aa49f821f83be7f42e275f4e5f
Frederico Novack <fredynovack@gmail.com>
2018-11-14 22:18:51 -0200

    Initialization with pods libs

---
+++ client/MapViewer/MapViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapViewer
+//
+//  Created by Frederico Novack on 14/11/18.
+//  Copyright © 2018 Novack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ client/MapViewer/Pods/Target Support Files/Pods-MapViewer/Pods-MapViewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapViewer : NSObject
+@end
+@implementation PodsDummy_Pods_MapViewer
+@end
