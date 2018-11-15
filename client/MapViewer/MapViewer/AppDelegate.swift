457449eb3aa5879cdbe8011d280d730bb6ed3f51
Frederico Novack <fredynovack@gmail.com>
2018-11-14 22:34:47 -0200

    Yey! Google Maps Working!

--- client/MapViewer/MapViewer/AppDelegate.swift
+++ client/MapViewer/MapViewer/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import GoogleMaps
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAvwfdDpPDtu7igBoQxcmSySPmAQc43tek")
         return true
     }
 
--- client/MapViewer/MapViewer/ViewController.swift
+++ client/MapViewer/MapViewer/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        mapView.camera = camera
+        
+
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+
     }
 
 
